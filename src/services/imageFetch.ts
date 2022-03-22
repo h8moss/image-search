4d5dca24d9da5109c86f9a970bfca03bdc75af88
h8m0ss <danarmenta32@gmail.com>
2022-03-22 13:52:44 -0600

    fixed image fetching

--- src/services/imageFetch.ts
+++ src/services/imageFetch.ts
@@ -1,28 +1,14 @@
 import { ImageModel } from "../types";
 
 const imageFetch = async (max: number): Promise<ImageModel[]> => {
-  let result = [];
+  const response = await fetch("/img_info.json");
+  const info = await response.json();
 
-  // while result.length < max, but keep a counter called i
-  for (let i = 0; result.length < max; i++) {
-    const response = await fetch(
-      `https://wallhaven.cc/api/v1/search?sorting=random&page=${i}`
-    );
-
-    // if we get timed out for example
-    if (response.status >= 400) {
-      break;
-    }
-
-    const body = JSON.parse(await response.text());
-    result.push({
-      url: body.url,
-      uploader: body.uploader,
-      tags: body.tags.map((t: any) => t.name),
-    });
-  }
-
-  return result;
+  return [...info].map((v) => ({
+    url: v.url,
+    tags: v.tags,
+    uploader: v.uploader,
+  }));
 };
 
 export default imageFetch;
